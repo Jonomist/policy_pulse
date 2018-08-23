af584bead8fb05de1f2a00179cd0962ca15a3ba9
Juliette Chevalier <juliettech.1393@gmail.com>
2018-08-23 12:07:37 +0800

    deleted introduction section and updated seeds to not get error message from general feedbacks

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts 'deleting general feedback'
+GeneralFeedback.destroy_all
 
 puts 'deleting all consultation'
 Consultation.destroy_all
